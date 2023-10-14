b6ae22799bf5016bd7c24e96adc9d1595a6081e6
neppiness <0414kjh@naver.com>
2023-10-14 17:54:22 +0900

    create: CompanyController (src/main/java/recruitment/controller)
    design.md 내용에 따라 회사 정보를 5가지 메소드로 처리하는 컨트롤러 구현
    - 등록, 아이디로 조회, 전체 조회, 삭제, 초기화

---
+++ src/main/java/recruitment/controller/CompanyController.java
@@ -0,0 +1,62 @@
+package recruitment.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import recruitment.domain.Company;
+import recruitment.repository.CompanyRepository;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Controller
+@RequestMapping(path="/company")
+public class CompanyController {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody Company addCompany(
+            @RequestParam String name,
+            @RequestParam String country,
+            @RequestParam String region
+    ) {
+        Company company = new Company();
+        company.setName(name);
+        company.setCountry(country);
+        company.setRegion(region);
+        companyRepository.save(company);
+        return company;
+    }
+
+    @GetMapping(path="/{companyId}")
+    public @ResponseBody Company findCompanyById(
+        @PathVariable long companyId
+    ) {
+        Optional<Company> foundCompany = companyRepository.findById(companyId);
+        if (foundCompany.isEmpty()) throw new NoSuchElementException();
+        return foundCompany.get();
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    @DeleteMapping(path="/{companyId}")
+    public @ResponseBody String deleteCompanyById(
+            @PathVariable long companyId
+    ) {
+        Optional<Company> foundCompany = companyRepository.findById(companyId);
+        if (foundCompany.isEmpty()) throw new NoSuchElementException();
+        companyRepository.deleteById(companyId);
+        return "The company is deleted (companyId: " + companyId + ")";
+    }
+
+    @DeleteMapping(path="/all")
+    public @ResponseBody String deleteAllCompanies() {
+        companyRepository.deleteAll();
+        return "All company data deleted";
+    }
+}
