01ed6107ef7dcaa213238c6013303282c30b0fe8
neppiness <0414kjh@naver.com>
2023-10-14 23:38:00 +0900

    update: JobRepository 인터페이스 메소드 추가 (src/main/java/recruiment/repository)
    Query 어노테이션을 활용한 JobRepository 내 3가지 find 메소드 구현

--- src/main/java/recruitment/repository/JobRepository.java
+++ src/main/java/recruitment/repository/JobRepository.java
@@ -1,8 +1,31 @@
 package recruitment.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import recruitment.domain.Job;
+
+import java.util.Collection;
 
 public interface JobRepository extends CrudRepository<Job, Long> {
 
+    @Query(value = "SELECT * FROM job j WHERE j.position = :position", nativeQuery = true)
+    Collection<Job> findJobsByPosition(
+            @Param("position") String position
+    );
+
+    @Query(value = "SELECT * FROM job j WHERE j.stack = :stack", nativeQuery = true)
+    Collection<Job> findJobsByStack(
+            @Param("stack") String stack
+    );
+
+    @Query(
+            value = "SELECT j.id, j.company_id, j.position, j.bounty, j.stack, j.description, c.name " +
+                    "FROM job AS j JOIN company AS c ON j.company_id = c.id " +
+                    "WHERE c.name = :companyName",
+            nativeQuery = true
+    )
+    Collection<Job> findJobsByCompanyName(
+            @Param("companyName") String companyName
+    );
 }
