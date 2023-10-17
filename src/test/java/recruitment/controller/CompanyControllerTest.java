85b4bba3046f9fd7352317f9db178f2ec63f9f37
neppiness <0414kjh@naver.com>
2023-10-17 17:54:56 +0900

    test: CompanyControllerTest 작성 (src/test/java/recruiment/controller)
    CompanyController의 메소드에 대한 테스트 작성
    - 현재 DB 상태와 무관하게 동작할 수 있는 스프링 통합 테스트 환경 구축

---
+++ src/test/java/recruitment/controller/CompanyControllerTest.java
@@ -0,0 +1,87 @@
+package recruitment.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+import recruitment.domain.Company;
+
+import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@ExtendWith(SpringExtension.class)
+public class CompanyControllerTest {
+
+    @Autowired
+    CompanyController companyController;
+
+    @Autowired
+    JobController jobController;
+
+    final static String senvexName = "센벡스";
+    final static String senvexCountry = "한국";
+    final static String senvexRegion = "서울 당산";
+
+    @BeforeEach
+    void companyTestSetup() {
+        jobController.deleteAllJobs();
+        companyController.deleteAllCompanies();
+        companyController.addCompany("원티드", "한국", "서울");
+        companyController.addCompany("네이버", "한국", "분당");
+    }
+
+    @Test
+    @DisplayName("회사 추가 및 회사 ID로 회사 검색 기능 테스트")
+    void addCompanyAndFindCompanyByIdTest() throws Exception {
+        Company company = companyController.addCompany(senvexName, senvexCountry, senvexRegion);
+        long companyId = company.getId();
+        Company foundCompany = companyController.findCompanyById(companyId);
+        assertThat(company).isEqualTo(foundCompany);
+    }
+
+    @Test
+    @DisplayName("등록된 모든 회사 조회 기능 테스트")
+    void getAllCompaniesTest() {
+        AtomicInteger count = new AtomicInteger();
+        Iterable<Company> allCompanies = companyController.getAllCompanies();
+        allCompanies.forEach(company -> {
+            StringBuilder sb = new StringBuilder();
+            count.getAndIncrement();
+            sb.append("company.getId()      = ").append(company.getId()).append('\n');
+            sb.append("company.getName()    = ").append(company.getName()).append('\n');
+            sb.append("company.getCountry() = ").append(company.getCountry()).append('\n');
+            sb.append("company.getRegion()  = ").append(company.getRegion()).append('\n');
+            System.out.println(sb);
+        });
+        assertThat(count.intValue()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("회사 ID로 객체 삭제 기능 테스트")
+    void deleteByCompanyIdTest() {
+        Company company = companyController.addCompany(senvexName, senvexCountry, senvexRegion);
+        long id = company.getId();
+        companyController.deleteCompanyById(id);
+        assertThatThrownBy(() -> {
+            companyController.findCompanyById(id);
+        }).isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    @DisplayName("등록된 모든 회사 삭제 기능 테스트")
+    void deleteAllCompaniesTest() {
+        System.out.println(companyController.deleteAllCompanies());
+        AtomicInteger count = new AtomicInteger();
+        Iterable<Company> allCompanies = companyController.getAllCompanies();
+        allCompanies.forEach(company -> count.getAndIncrement());
+        assertThat(count.intValue()).isEqualTo(0);
+    }
+}
