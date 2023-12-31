7898d48571f055aecb65ad7c3b023cda83c7954c
neppiness <0414kjh@naver.com>
2024-01-01 01:35:26 +0900

    Refactor: UserCustomRepository를 JPA Criteria로 구현

--- src/main/java/recruitment/repository/UserRepository.java
+++ src/main/java/recruitment/repository/UserRepository.java
@@ -1,13 +1,7 @@
 package recruitment.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import recruitment.domain.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByNameAndPassword(String name, String password);
-
-    Optional<User> findByName(String name);
-
+public interface UserRepository extends JpaRepository<User, Long>, UserCustomRepository {
 }
