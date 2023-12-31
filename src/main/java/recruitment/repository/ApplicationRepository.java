c30abe58fae4408e012a062ff7ce79931acd8d1c
neppiness <0414kjh@naver.com>
2024-01-01 01:43:03 +0900

    Refactor: ApplicationRepository에 사용하지 않는 메소드 삭제

--- src/main/java/recruitment/repository/ApplicationRepository.java
+++ src/main/java/recruitment/repository/ApplicationRepository.java
@@ -1,31 +1,7 @@
 package recruitment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import recruitment.domain.Application;
 
-import java.util.Collection;
-import java.util.Optional;
-
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
-
-    @Query(value = "SELECT * FROM application a WHERE a.user_id = :userId AND a.job_id = :jobId", nativeQuery = true)
-    Optional<Application> findApplicationByUserIdAndJobId(
-            @Param("userId") Long userId,
-            @Param("jobId") Long jobId
-    );
-
-    @Query(value = "SELECT * FROM application a WHERE a.user_id = :userId", nativeQuery = true)
-    Collection<Application> findApplicationsByUserId(
-            @Param("userId") Long userId
-    );
-
-    @Modifying
-    @Query(value = "DELETE FROM application a WHERE a.user_id = :userId", nativeQuery = true)
-    void deleteApplicationsByUserId(
-            @Param("userId") Long userId
-    );
-
 }
